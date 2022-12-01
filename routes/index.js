6c416e878992d94f70dfabe635a9e7d43acf1533
hansburnthem <hnifrma@gmail.com>
2022-12-01 20:42:45 +0700

    halfway

--- routes/index.js
+++ routes/index.js
@@ -42,7 +42,29 @@ router.use('/import-data', async (req, res) => {
 })
 
 router.use('/edit-repacking-data', async (req, res) => {
-  res.json({statusCode: 1, message: 'stock_read_log.json imported!'})
+  /**
+   * @type {stock_read_log}
+   */
+  const data = await stock_read_log.findOne({payload: req.body.payload})
+  if(data === null) {
+    res.json({statusCode: 0, message: 'No Data'})
+  }
+
+  /**
+   * @type {Array}
+   */
+  let qrData = data.qr_list
+  for (let i = 0; i < 1; i++) {
+    const element = req.body.new_qr_list[i];
+    let dataNew = await stock_read_log.findOne({qr_list: {$elemMatch: {payload: element.payload}}})
+    qrData.push(dataNew.qr_list.find(el => el.payload === element.payload))
+    // dataNew.qr_list.splice(dataNew.qr_list.indexOf(element.payload), 1)
+    dataNew.qr_list = dataNew.qr_list.filter(el => el.payload !== element.payload)
+    dataNew.qty = dataNew.qty - 1
+    // await stock_read_log.updateOne({qr_list: {$elemMatch: {payload: element.payload}}})
+    dataNew.save()
+  }
+  res.json({statusCode: 1, data: qrData})
 
 })
 
